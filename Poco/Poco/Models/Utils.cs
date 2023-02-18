0cf8adb1a8615016d22640fc53b8b093765ecc86
Wati-Dev <raphaelgrougnetca@gmail.com>
2023-02-18 17:06:53 -0500

    Ajout des fonctions de Utils

--- Poco/Poco/Models/Utils.cs
+++ Poco/Poco/Models/Utils.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace Poco.Models
 {
-    public class Utils
+    public static class Utils
     {
 
         #region CONSTANTES
@@ -26,7 +29,79 @@ namespace Poco.Models
         #endregion
 
         #region MÉTHODES
+        /// <summary>
+        /// Initialiser le fichier de trace
+        /// </summary>
+        static public void InitialiserFichierTrace()
+        {
+            Stream leFichier = File.Create("FichierTrace.txt");
+            TextWriterTraceListener leListener = new TextWriterTraceListener(leFichier);
+            Trace.AutoFlush = true;
+            Trace.Listeners.Add(leListener);
+        }
 
+
+        /// <summary>
+        /// Tracer un message dans le fichier de trace
+        /// </summary>
+        /// <param name="pMsg">Message à écrire</param>
+        static public void Tracer(string pMsg)
+        {
+            Trace.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}> {pMsg}");
+        }
+
+
+
+        static public List<string[]>? ChargerDonnees(String pCheminFichier)
+        {
+            if (File.Exists(pCheminFichier))
+            {
+                StreamReader fluxLecture = new StreamReader(pCheminFichier);
+                String fichierTexte = fluxLecture.ReadToEnd();
+                fluxLecture.Close();
+                fichierTexte = fichierTexte.Replace("\r", "");
+
+                // Création d'un vecteur de chaînes de caractères contenant chaque ligne individuellement.
+                String[] vectLignes = fichierTexte.Split('\n');
+                
+
+                // Nombre de lignes non vides dans le fichier.
+                int nbLignes;
+
+                if (vectLignes[vectLignes.Length - 1] == "")
+                    nbLignes = vectLignes.Length - 1; // On ne considère pas la dernière ligne si elle est vide.
+                else
+                    nbLignes = vectLignes.Length;
+
+                if (nbLignes > 0)
+                {
+                    // Création du vecteur contenant les données du fichier; la taille est déterminée par le nombre de lignes (non vides) du fichier.
+                    List<string[]> ListLignes = new List<string[]>();
+
+                    for (int i = 0; i < nbLignes; i++)
+                    {
+                        ListLignes.Add(vectLignes[i].Split(";"));
+                    }
+
+                    // On retourne le vecteur contenant les données créé.
+                    ListLignes.RemoveAt(0);
+
+                    return ListLignes;
+
+                }
+            }
+
+            return null;
+        }
+
+        public static void EnregistrerDonnees(String pCheminFichier, string pDonneesSerialises)
+        {
+
+            StreamWriter fluxEcriture = new StreamWriter(pCheminFichier, true);
+            fluxEcriture.Write(pDonneesSerialises+'\n');
+            fluxEcriture.Close();
+
+        }
         #endregion
 
     }
