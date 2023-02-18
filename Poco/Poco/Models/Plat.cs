37be49749f97e161974e1c7be967be33b384f4e0
Wati-Dev <raphaelgrougnetca@gmail.com>
2023-02-18 16:26:49 -0500

    Ajout des classes principales

---
+++ Poco/Poco/Models/Plat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poco.Models
+{
+    public enum TypePlat
+    {
+        Burrito,
+        Fajita,
+        Nachos,
+        Quesadilla,
+        Tacos
+    }
+
+    public class Plat
+    {
+    }
+}
