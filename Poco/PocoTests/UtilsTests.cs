d92dd1fc137063ee689abce309f0f12f18f6dfa3
Wati-Dev <raphaelgrougnetca@gmail.com>
2023-02-23 11:36:27 -0500

    Tests

--- Poco/PocoTests/UtilsTests.cs
+++ Poco/PocoTests/UtilsTests.cs
@@ -1,4 +1,6 @@
 using Poco.Models;
+using System;
+using System.IO;
 
 namespace PocoTests
 {
@@ -9,7 +11,8 @@ namespace PocoTests
         {
             List<string[]> list = new List<string[]>();
             list = Utils.ChargerDonnees("C:\\Users\\rapha\\Desktop\\Poco Projet Suicide\\poco\\Poco\\Poco\\Files\\Factures.csv");
-
+            string env = Environment.CurrentDirectory;
+            string path = Directory.GetParent(env).Parent.Parent.FullName;
             Plat plat = new Plat(TypePlat.Burrito);
             
         }
