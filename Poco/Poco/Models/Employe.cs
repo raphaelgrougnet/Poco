8e278151743960bee3a48e0e01cf4f173c680a7c
Wati-Dev <raphaelgrougnetca@gmail.com>
2023-02-23 10:54:02 -0500

    Remplissage des classes

--- Poco/Poco/Models/Employe.cs
+++ Poco/Poco/Models/Employe.cs
@@ -8,25 +8,83 @@ namespace Poco.Models
 {
     public class Employe
     {
-
+        
         #region CONSTANTES
 
         #endregion
 
         #region ATTRIBUTS
+        private string _code;
 
+        private string _nom;
+
+        private string _prenom;
+
+        private DateTime _dateNaissance;
+
+        private TimeSpan _heuresTravailles;
+
+        private List<Poincon> _mesPoincons;
+        
         #endregion
 
         #region PROPRIÉTÉS
+        public string Code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+        public string Nom
+        {
+            get { return _nom; }
+            set { _nom = value; }
+        }
+
+        public string Prenom
+        {
+            get { return _prenom; }
+            set { _prenom = value; }
+        }
+
+        public DateTime DateNaissance
+        {
+            get { return _dateNaissance; }
+            set { _dateNaissance = value; }
+        }
+
+        public TimeSpan HeuresTravailles
+        {
+            get { return _heuresTravailles; }
+            set { _heuresTravailles = value; }
+        }
+
+        public List<Poincon> MesPoincons
+        {
+            get { return _mesPoincons; }
+            set { _mesPoincons = value; }
+        }
+
 
         #endregion
 
         #region CONSTRUCTEURS
+        public Employe(string code, string nom, string prenom, DateTime dateNaissance, TimeSpan heuresTravailles, List<Poincon> mesPoincons)
+        {
+            Code = code;
+            Nom = nom;
+            Prenom = prenom;
+            DateNaissance = dateNaissance;
+            HeuresTravailles = heuresTravailles;
+            MesPoincons = mesPoincons;
+        }
 
         #endregion
 
         #region MÉTHODES
-
+        public override string ToString()
+        {
+            return Nom + " " + Prenom;
+        }
         #endregion
 
     }
--- Poco/Poco/Models/Facture.cs
+++ Poco/Poco/Models/Facture.cs
@@ -14,19 +14,91 @@ namespace Poco.Models
         #endregion
 
         #region ATTRIBUTS
+        private uint _noFacture;
+
+        private DateTime _date;
+
+        private List<Plat> _listePlats;
+
+        private decimal _sousTotal;
+
+        private decimal _prixTotal;
+
+        
+
 
         #endregion
 
         #region PROPRIÉTÉS
+        
+        public uint NoFacture
+        {
+            get { return _noFacture; }
+            set { _noFacture = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        public List<Plat> ListePlats
+        {
+            get { return _listePlats; }
+            set { _listePlats = value; }
+        }
+
+        public decimal SousTotal
+        {
+            get
+            {
+                SousTotal = CalculerSousTotal();
+                return _sousTotal;
+            }
+            private set { _sousTotal = value; }
+        }
+
+        public decimal PrixTotal
+        {
+            get 
+            {
+                PrixTotal = CalculerPrixTotal();
+                return _prixTotal; 
+            }
+            private set { _prixTotal = value; }
+        }
+
 
         #endregion
 
         #region CONSTRUCTEURS
-
+        public Facture(uint noFacture, DateTime date, List<Plat> listePlats, decimal sousTotal, decimal prixTotal)
+        {
+            NoFacture = noFacture;
+            Date = date;
+            ListePlats = listePlats;
+            SousTotal = sousTotal;
+            PrixTotal = prixTotal;
+        }
         #endregion
 
         #region MÉTHODES
+        private decimal CalculerSousTotal()
+        {
+            decimal sousTotal = 0;
+            foreach (Plat plat in ListePlats)
+            {
+                sousTotal += plat.Prix;
+            }
+            return sousTotal;
+        }
 
+        private decimal CalculerPrixTotal()
+        {
+            decimal prixTotal = SousTotal + (SousTotal * 0.15m);
+            return prixTotal;
+        }
         #endregion
 
     }
