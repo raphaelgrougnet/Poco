c29b81db865256238da9fa16437ca64aa48be70c
Wati-Dev <raphaelgrougnetca@gmail.com>
2023-03-23 18:18:02 -0400

    Ajout du binding

--- Poco/Poco/Models/Facture.cs
+++ Poco/Poco/Models/Facture.cs
@@ -110,5 +110,7 @@ namespace Poco.Models
         }
         #endregion
 
+
+        
     }
 }
--- Poco/Poco/Views/FormFacture.xaml.cs
+++ Poco/Poco/Views/FormFacture.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Poco.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,17 @@ namespace Poco.Views
         public FormFacture()
         {
             InitializeComponent();
+            Plat plat = new Plat(TypePlat.Nachos);
+            plat.AjouterGarniture(new Legume("Mais"));
+            plat.AjouterGarniture(new Legume("Oignon"));
+            plat.AjouterGarniture(new Legume("Poivron"));
+            plat.AjouterGarniture(new Legume("Tomate"));
+
+            Facture f = new Facture(1, DateTime.Today, new List<Plat>() { plat, plat }, 25.0m, 25.3m);
+            
+            
+            lstFacture.ItemsSource = f.ListePlats;
+            
         }
     }
 }
