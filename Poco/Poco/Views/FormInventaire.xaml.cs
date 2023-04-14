d9f24b9a75b628b8e3c3bd6bd16ec1bb3961fc38
Julien Paquet-Beaudoin <paqj8978@gmail.com>
2023-04-14 10:55:58 -0400

    Ajout du formulaire FormInventaire

---
+++ Poco/Poco/Views/FormInventaire.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Poco.Views
+{
+    /// <summary>
+    /// Interaction logic for FormInventaire.xaml
+    /// </summary>
+    public partial class FormInventaire : Window
+    {
+        public FormInventaire()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+"); 
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+    }
+}
