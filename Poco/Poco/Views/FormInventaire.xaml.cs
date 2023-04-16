2078320b3f2ea164d5a60bb8fc326190d1f1946e
Julien Paquet-Beaudoin <paqj8978@gmail.com>
2023-04-16 12:52:38 -0400

    Ajout du style, affichage de l'inventaire et button pour fermer le Form

--- Poco/Poco/Views/FormInventaire.xaml.cs
+++ Poco/Poco/Views/FormInventaire.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Poco.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,13 @@ namespace Poco.Views
     /// </summary>
     public partial class FormInventaire : Window
     {
-        public FormInventaire()
+        private Dictionary<Garniture, int> _inventaire;
+
+        public FormInventaire(Dictionary<Garniture, int> pInventaire)
         {
             InitializeComponent();
+            _inventaire = pInventaire;
+            InitialiserForm();
         }
         private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
@@ -30,5 +35,93 @@ namespace Poco.Views
             e.Handled = regex.IsMatch(e.Text);
         }
 
+        private void InitialiserForm()
+        {
+            txtQuantite.Text = "0";
+            GestionBtnAjouter();
+            AfficherListeGarniture();
+
+        }
+
+        private void AfficherListeGarniture()
+        {
+            lstGarniture.Items.Clear();
+
+            foreach (KeyValuePair<Garniture, int> garniture in _inventaire)
+            {
+                lstGarniture.Items.Add(garniture.Key.Nom + " - " + garniture.Value);
+            }
+        }
+
+        private void GestionBtnAjouter()
+        {
+            int quantite;
+
+            if (txtQuantite.Text == "")
+                quantite = 0;
+            else
+             quantite = int.Parse(txtQuantite.Text);
+
+            if (lstGarniture.SelectedItem != null)
+            {
+                if (quantite == 0)
+                {
+                    btnAjouter.IsHitTestVisible = false;
+                }
+                else
+                {
+                    btnAjouter.IsHitTestVisible = true;
+                }
+
+            }
+            else
+            {
+                btnAjouter.IsHitTestVisible = false;
+            }
+
+            AfficherBtnAjouter();
+
+        }
+
+        private void AfficherBtnAjouter()
+        {
+            if (!btnAjouter.IsHitTestVisible)
+            {
+                btnAjouter.BorderBrush = new SolidColorBrush(Color.FromRgb(42,42,42));
+                btnAjouter.Background = new SolidColorBrush(Color.FromRgb(244, 244, 244));
+                (btnAjouter.Child as Label).Foreground = new SolidColorBrush(Color.FromRgb(132, 131, 131));
+
+                brdQuantite.Background = new SolidColorBrush(Color.FromRgb(244, 244, 244));
+                brdQuantite.BorderBrush = new SolidColorBrush(Color.FromRgb(42, 42, 42));
+                txtQuantite.Background = new SolidColorBrush(Color.FromRgb(244, 244, 244));
+                txtQuantite.Foreground = new SolidColorBrush(Color.FromRgb(132, 131, 131));
+            }
+            else
+            {
+                btnAjouter.BorderBrush = new SolidColorBrush(Color.FromRgb(196, 87, 24));
+                btnAjouter.Background = new SolidColorBrush(Color.FromRgb(253, 201, 3));
+                (btnAjouter.Child as Label).Foreground = new SolidColorBrush(Colors.Black);
+
+                brdQuantite.Background = new SolidColorBrush(Color.FromRgb(254, 225, 111));
+                brdQuantite.BorderBrush = new SolidColorBrush(Color.FromRgb(196, 87, 24));
+                txtQuantite.Background = new SolidColorBrush(Color.FromRgb(254, 225, 111));
+                txtQuantite.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void lstGarniture_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GestionBtnAjouter();
+        }
+
+        private void txtQuantite_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            GestionBtnAjouter();
+        }
+
+        private void btnFermer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
     }
 }
